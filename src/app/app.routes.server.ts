910fa91a1d104929f0b549c05b8fbba878bf929b
ronniestark999@gmail.com <ronniestark999@gmail.com>
2026-09-12 21:27:44 -0600

    fix render

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,20 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'catalogo/:categoria',
+    renderMode: RenderMode.Prerender,
+    // ¡Agregamos 'async' aquí!
+    getPrerenderParams: async () => { 
+      return [
+        { categoria: 'figuras' },
+        { categoria: 'llaveros' },
+        { categoria: 'utensilios-cocina' },
+        { categoria: 'herramientas' },
+        { categoria: 'repuestos-auto' }
+      ];
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
